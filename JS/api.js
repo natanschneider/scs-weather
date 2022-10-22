fe0084cf011420ac83eb92c20447e1b0bf7bf719
Natãn Schneider <natanschneider@protonmail.com>
2022-10-22 17:45:45 -0300

    function call

--- JS/api.js
+++ JS/api.js
@@ -6,8 +6,10 @@ async function getAPI(){
     let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=-29.717778&lon=-52.425833&lang=pt_br&appid=bc4c64993f3989bff1bc2a4b87192c4c&units=metric");
     
     let data = await response.json();
+
+    writeHTML(data);
 }
 
-function writeHTML(){
-
+function writeHTML(data){
+    
 }
