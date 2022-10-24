f8114135b9a37fd722814113a3190402830ae6eb
Natãn Schneider <natanschneider@protonmail.com>
2022-10-23 21:16:22 -0300

    writeHTML function

--- JS/api.js
+++ JS/api.js
@@ -1,9 +1,11 @@
+let OWtoken = "";
+
 window.onload = async()=>{
-    
+    getAPI(OWtoken);
 }
 
-async function getAPI(){
-    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=-29.717778&lon=-52.425833&lang=pt_br&appid=bc4c64993f3989bff1bc2a4b87192c4c&units=metric");
+async function getAPI(appId){
+    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=-29.717778&lon=-52.425833&lang=pt_br&units=metric&appid=" + appId);
     
     let data = await response.json();
 
@@ -11,5 +13,13 @@ async function getAPI(){
 }
 
 function writeHTML(data){
-    
+    document.getElementById('main') = data['weather'][0]['description'] + '°C';
+    document.getElementById('temperature') = data['main']['temp'] + '°C';
+    document.getElementById('humidity') = 'Umidade: ' + data['main']['humidity'] + '%';
+    document.getElementById('visibility') = 'Visibilidade: ' + parseFloat(data['visibility'] / 0.001) + 'km';
+    document.getElementById('wind') = 'Vento: ' + data['wind']['speed'] + ' m/s';
+    document.getElementById('direction') = 'Direção: ' + data['wind']['deg'] + '°';
+
+    let icon = "http://openweathermap.org/img/wn/" + data['weather'][0]['icon'] + "@2x.png";
+    document.getElementById('icon').src = icon;
 }
