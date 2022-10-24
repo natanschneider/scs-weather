dc95343742c4027a9058094229bdd80e30745423
Natãn Schneider <natanschneider@protonmail.com>
2022-10-23 23:13:36 -0300

    commit

--- JS/api.js
+++ JS/api.js
@@ -1,25 +1,21 @@
-let OWtoken = "";
-
 window.onload = async()=>{
-    getAPI(OWtoken);
+    writeHTML();
 }
 
-async function getAPI(appId){
-    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=-29.717778&lon=-52.425833&lang=pt_br&units=metric&appid=" + appId);
+async function writeHTML(){
+    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=-29.717778&lon=-52.425833&lang=pt_br&units=metric&appid=");
     
     let data = await response.json();
+    
+    document.getElementById('main').innerHTML = data['weather'][0]['description'];
+    document.getElementById('temperature').innerHTML = data['main']['temp'] + '°C';
+    document.getElementById('humidity').innerHTML = 'Umidade: ' + data['main']['humidity'] + '%';
+    document.getElementById('wind').innerHTML = 'Vento: ' + data['wind']['speed'] + ' m/s';
+    document.getElementById('direction').innerHTML = 'Direção: ' + data['wind']['deg'] + '°';
 
-    writeHTML(data);
-}
-
-function writeHTML(data){
-    document.getElementById('main') = data['weather'][0]['description'] + '°C';
-    document.getElementById('temperature') = data['main']['temp'] + '°C';
-    document.getElementById('humidity') = 'Umidade: ' + data['main']['humidity'] + '%';
-    document.getElementById('visibility') = 'Visibilidade: ' + parseFloat(data['visibility'] / 0.001) + 'km';
-    document.getElementById('wind') = 'Vento: ' + data['wind']['speed'] + ' m/s';
-    document.getElementById('direction') = 'Direção: ' + data['wind']['deg'] + '°';
+    let visibilidade = (parseInt(data['visibility']) * 0.001);
+    document.getElementById('visibility').innerHTML = 'Visibilidade: ' + visibilidade + ' km';
 
     let icon = "http://openweathermap.org/img/wn/" + data['weather'][0]['icon'] + "@2x.png";
-    document.getElementById('icon').src = icon;
+    document.getElementById('icone').src = url(icon);
 }
